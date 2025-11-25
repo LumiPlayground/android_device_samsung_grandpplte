a8b5190118683a48378fe58d388440fba0b1035f
nhglong <its.nhglong@gmail.com>
2025-11-25 18:10:21 +0700

    grandpplte: Welcome to LineageOS!
    Change-Id: I949275ef577108953600e38e035fff80977a5184

--- lineage.mk
+++ lineage.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from grandpplte device
 $(call inherit-product, device/samsung/grandpplte/device.mk)
