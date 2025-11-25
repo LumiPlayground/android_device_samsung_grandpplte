cb01d9117fe153c33807ebe26db85ffc296c4adf
nhglong <its.nhglong@gmail.com>
2025-11-25 21:56:25 +0700

    grandpplte: Configure hardware buttons using overlay
    Change-Id: I595307af9cc686c08d69262f79de43eeebdb30de

--- device.mk
+++ device.mk
@@ -130,7 +130,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
